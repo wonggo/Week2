f7d1e290acc7e836b117bb297de4a32bf41dd017
William Onggo <William Onggo>
2017-03-06 12:36:15 +0700

    inilah saat terakhirku

---
+++ resources/views/bebas.blade.php
@@ -0,0 +1,3 @@
+Hallo Website Development
+
+<br><img src="{{ asset ('image/9f539fd6798afe429ae23160af4090da.jpg')}}"></br>
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+	$tanggal = date("d-m-Y H:i:s",time());
+	$gini = "ia gitu";
+	return view ('Welcome')
+	->with('tanggal', $tanggal)
+	->with('gini', $gini);
+});
+
+Route::get('hola', function(){
+	return view('halo');
+});
+
+route::get('bebas',function(){
+	return view('bebas');
+});
+route::get('hola/bebas',function(){
+	return view('terserah.bebas');
+    
+});
